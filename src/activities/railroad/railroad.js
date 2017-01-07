39dae9672d30cfc71b9048f2c772be59633a0ee0
Utkarsh Tiwari <iamutkarshtiwari@gmail.com>
2017-01-07 21:31:12 +0530

    Author details updated

--- src/activities/railroad/railroad.js
+++ src/activities/railroad/railroad.js
@@ -1,10 +1,10 @@
 /* GCompris - railroad.js
  *
- * Copyright (C) 2016 YOUR NAME <xx@yy.org>
+ * Copyright (C) 2016 Utkarsh Tiwari <iamutkarshtiwari@kde.org>
  *
  * Authors:
- *   <THE GTK VERSION AUTHOR> (GTK+ version)
- *   "YOUR NAME" <YOUR EMAIL> (Qt Quick port)
+ *   <Pascal Georges> (GTK+ version)
+ *   "Utkarsh Tiwari" <iamutkarshtiwari@kde.org> (Qt Quick port)
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
